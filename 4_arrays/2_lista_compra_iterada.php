b26afb38cacbae9a48d6be8e590386434007e048
Jhonny <caballoloco.jg@gmail.com>
2025-02-27 17:37:15 +0100

    agenda 4_arrays

--- 4_arrays/2_lista_compra_iterada.php
+++ 4_arrays/2_lista_compra_iterada.php
@@ -3,3 +3,8 @@ $compra=["pan","cafe","agua","leche"];
 for ($i=0; $i <count($compra); $i++) { 
     echo($compra[$i])."<br>";
 }
+
+foreach ($compra as $producto) {
+    echo"<imput type='checkbox'>".$producto."<br>";
+}
+echo"<hr>";
---
+++ 4_arrays/3_agenda.php
@@ -0,0 +1,6 @@
+<?php
+$agenda=["paco"=>654765234,"pedro"=>675098235,"manolo"=>643098475];
+var_dump($agenda);
+foreach ($agenda as $nombre => $numero) {
+    echo($nombre:$numero);
+}
