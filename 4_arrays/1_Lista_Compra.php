44787cc1b78c1e885decb7875ddf6a8f639c942b
Jhonny <caballoloco.jg@gmail.com>
2025-02-27 17:09:37 +0100

    arrays se enter the commit message for your changes. Lines starting

---
+++ 4_arrays/1_Lista_Compra.php
@@ -0,0 +1,6 @@
+<?php
+$Compra = ["cafe","agua","leche","pan"];
+var_dump($Compra);
+$Compra[2]="vino";
+var_dump($Compra);
+?>
---
+++ 4_arrays/2_lista_compra_iterada.php
@@ -0,0 +1,5 @@
+<?php
+$compra=["pan","cafe","agua","leche"];
+for ($i=0; $i <count($compra); $i++) { 
+    echo($compra[$i])."<br>";
+}
