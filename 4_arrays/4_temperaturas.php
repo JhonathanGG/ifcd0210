54d67cb5240230b0d47ca0201ed8d5497f56b34e
Jhonny <caballoloco.jg@gmail.com>
2025-02-27 19:22:38 +0100

    4_temperaturas

---
+++ 4_arrays/4_temperaturas.php
@@ -0,0 +1,44 @@
+<?php
+
+/* 
+Dado un array con la temperatura media de  cada día de la semana, sacar:
+ - la temperatura máxima, 
+ - la mínima 
+ - y la temperatura media
+
+*/
+
+$temperaturas = [12, 15, 13, 12, 16, 11, 13];
+
+$temperatura_maxima = max($temperaturas);
+
+$temperatura_minima = min($temperaturas);
+
+$temperatura_media = array_sum($temperaturas) / count($temperaturas);
+
+echo($temperatura_media);
+
+?>
+
+<?php
+$temperaturas = [12, 15, 13, 12, 16, 11, 13];
+
+$temperatura_maxima = $temperaturas[0]; 
+$temperatura_minima = $temperaturas[0];  
+$suma_temperaturas = 0; 
+
+for ($i = 0; $i < count($temperaturas); $i++) {
+        if ($temperaturas[$i] > $temperatura_maxima) {
+        $temperatura_maxima = $temperaturas[$i];  
+    }
+        if ($temperaturas[$i] < $temperatura_minima) {
+        $temperatura_minima = $temperaturas[$i];  
+    }    
+        $suma_temperaturas += $temperaturas[$i];  
+}
+$temperatura_media = $suma_temperaturas / count($temperaturas); 
+echo "Temperatura máxima: $temperatura_maxima °C\n";
+echo "Temperatura mínima: $temperatura_minima °C\n";
+echo "Temperatura media: $temperatura_media °C\n";
+?>
+
