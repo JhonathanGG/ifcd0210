609bb4efd9c96e821e4b04e88c5939262608e05d
Jhonny <caballoloco.jg@gmail.com>
2025-03-26 19:20:42 +0100

    .

---
+++ 6_Formularios/3_Login/login.php
@@ -0,0 +1,14 @@
+<?php
+
+
+// comprobar si los valores recibidos del formulario coinciden con los siguientes:
+$usuario = 'admin';
+$password = '$uper$ecr3t';
+// si coinciden, redirigir a la página de bienvenida
+// sino, redirigir a la página de error
+// pista: la función header('Location: nombre_del_fichero.extension') sirve para redirigir
+if ($usuario === $_POST['usuario'] && $password === $POST['password']) {
+    header("Location: bienvenida.php?usuario = $_POST[usuario]");   
+} else {
+    header("Location: error.html");    
+}
